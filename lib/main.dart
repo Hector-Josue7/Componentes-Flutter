05d67ef57b607474edf01cce19124a873a147cb2
Hector-Josue7 <hectorardonm@gmail.com>
2021-02-03 17:52:05 -0600

    Se borraron los tres ListTile del home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // se utiliza el snippet mateapp para construir la interfaz basica del main.dart
 import 'package:flutter/material.dart';
+import 'package:componentes/src/paginas/home_temp.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,18 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Componentes App',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Componentes'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+        title: 'Componentes App',
+        debugShowCheckedModeBanner: false,
+        home: HomePageTemp());
   }
 }
--- lib/src/paginas/home_temp.dart
+++ lib/src/paginas/home_temp.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 // el snippet a utilizar es stle -> Flutter stateless widget
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
   @override
   Widget build(BuildContext context) {
-    return Center(
-       child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('ListTile Titulo axn'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Titulo axn'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Titulo axn'),
+          )
+        ],
+      ),
     );
   }
 }
